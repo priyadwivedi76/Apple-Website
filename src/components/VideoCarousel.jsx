c2acc9cac6b082f265272b9ba80303eb4d8127f3
Dwivedi Priya <pd7668665@gmail.com>
2025-03-20 21:01:41 +0530

    video-play

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,6 +1,10 @@
 import React,{useEffect, useRef, useState} from 'react'
 import {hightlightsSlides} from '../constants'
 import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+// Register the plugin
+gsap.registerPlugin(ScrollTrigger);
 import { pauseImg, playImg, replayImg } from '../utils';
 const VideoCarousel = () => {
   // 3.initialise the useRef and the useState variable according to the conditions
@@ -22,18 +26,43 @@ const VideoCarousel = () => {
   // 4.Destructing
   const {isEnd,startPlay,videoId,isLastVideo,isPlaying}=video;
 
-
-  // 6.Deals with playing of the video
+  //10.Animation for each video to start
   useEffect(()=>{
-    if(loadedData>3){
-      if(!isPlaying){
-        videoRef.current[videoId].pause();;
-      }else{
-        startPlay && videoRef.current[videoId].play();
+    gsap.to('#video',{
+      scrollTrigger:{
+        trigger:'#video',
+        toggleActions:'restart none none none'
+      },
+      onComplete:()=>{
+        setVideo((pre)=>({
+          ...pre,
+          startPlay:true,
+          isPlaying:true,
+        }))
+      }
+    })
+  },[isEnd,videoId])
+
+  useEffect(() => {
+    if (videoRef.current[videoId]) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        videoRef.current[videoId].play();
       }
     }
+  }, [startPlay, videoId, isPlaying, loadedData]);
 
-  },[startPlay,videoId,isPlaying,loadedData])
+  // 6.Deals with playing of the video
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId]?.pause();
+      } else {
+        startPlay && videoRef.current[videoId]?.play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
 
   // 5.useeffect:changes when the videoId changes 
   useEffect(()=>{
@@ -54,6 +83,7 @@ const VideoCarousel = () => {
   },[videoId,startPlay])
 
 
+  // 9.switch case that manages various states of the image and their state must change after the condition are fulfilled
   const handleProcess = (type, i) => {
     switch (type) {
       case "video-end":
@@ -81,7 +111,10 @@ const VideoCarousel = () => {
     }
   };
 
-  
+  //11.function to handle loaded data
+  const handleLoadedMetaData=(i,e)=>{
+    setLoadedData((pre)=>({...pre,e}))
+  }
   return (
     <>
     <div className='flex items-center'>
@@ -91,7 +124,7 @@ const VideoCarousel = () => {
         <div key={i} id='slider' className='sm:pr-20 pr-10'>
           <div className='video-carousel_container'>
             <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
-              <video id='video' playsInline={true} preload='auto' muted ref={(el)=>(videoRef.current[i]=el)} onPlay={()=>{setVideo((prev)=>({...prev,isPlaying:true}))}} >
+              <video id='video' playsInline={true} preload='auto' muted ref={(el)=>(videoRef.current[i]=el)}  onPlay={() => { setVideo((prev) => ({ ...prev, isPlaying: true })) }} onLoadedMetadata={(e)=>handleLoadedMetaData(i,e)}>
                 <source src={highlightSlide.video} type='video/mp4' />
               </video>
             </div>
