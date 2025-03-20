4a9f5ab032a94ec65d0a23f75f974e8bd41717d0
Dwivedi Priya <pd7668665@gmail.com>
2025-03-20 18:36:13 +0530

    highlight-section-layout

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import {hightlightsSlides} from '../constants'
 const VideoCarousel = () => {
   return (
     <>
-    
+    <div className='flex items-center'>
+      {hightlightsSlides.map((highlightSlide,i)=>(
+        <div key={i} id='slider' className='sm:pr-20 pr-10'>
+          <div className='video-carousel_container'>
+            <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+              <video id='video' playsInline={true} preload='auto' muted>
+                <source src={highlightSlide.video} type='video/mp4'/>
+              </video>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
     </>
   )
 }
